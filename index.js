99247bc82af333732672cef626cc5ac8e72205a6
Pob <590650@gmail.com>
2018-09-15 03:15:43 +0700

    bind morgan and bodyParser middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const router = require('./router')
 
 const app = express()
 
-
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+router(app)
 
 const port = process.env.PORT || 3090
-app.listen(port)
-console.log('Server listening on:', port)
+app.listen(port, () => { console.log(`Server listening on: ${port}`) })
